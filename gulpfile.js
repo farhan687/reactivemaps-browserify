cd0e6536f9e13ceded218dc6502940cdab6aadb3
farhan687 <farhan687@gmail.com>
2017-02-08 16:32:21 +0530

    Use reactivemaps

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,18 @@ var concat = require('gulp-concat');
 var files = {
 	css: {
 		vendor: [
-			'node_modules/sensor-js/dist/css/vendor.min.css'
-		]
-	},
-	js: {
-		vendor: [
-			'node_modules/sensor-js/dist/js/vendor.min.js'
+			'node_modules/@appbaseio/reactivemaps/dist/css/style.min.css'
 		]
 	}
 };
+
+
+gulp.task('moveFonts', function() {
+	return gulp.src([
+			'node_modules/@appbaseio/reactivemaps/dist/fonts/**/*'
+		])
+		.pipe(gulp.dest('dist/fonts'));
+});
 
 gulp.task('vendorcss', function() {
 	return gulp.src(files.css.vendor)
@@ -20,15 +23,9 @@ gulp.task('vendorcss', function() {
 		.pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('vendorjs', function() {
-	return gulp.src(files.js.vendor)
-		.pipe(concat('vendor.min.js'))
-		.pipe(gulp.dest('dist/js'));
-});
-
 gulp.task('compact', [
 	'vendorcss',
-	'vendorjs'
+	'moveFonts'
 ]);
 
 gulp.task('default', ['compact']);
